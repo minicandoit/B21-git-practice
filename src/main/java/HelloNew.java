8aa31188ab65372511fbc9254506d13e77e02f00
minicandoit <74155239+minicandoit@users.noreply.github.com>
2021-01-27 19:56:43 -0500

    initial HelloNew for conflict

--- src/main/java/HelloNew.java
+++ src/main/java/HelloNew.java
@@ -2,5 +2,6 @@ public class HelloNew {
     public static void main(String[] args) {
         System.out.println("Hello World B22");
         System.out.println("B21 future SDET learning Git");
+        System.out.println("more change for conflict");
     }
 }
