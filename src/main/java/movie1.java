52ed47ba90c6807be47ea8e0ebc8c09a9f61603f
minicandoit <74155239+minicandoit@users.noreply.github.com>
2021-01-27 19:46:33 -0500

    final movie for 10

--- src/main/java/movie1.java
+++ src/main/java/movie1.java
@@ -2,5 +2,6 @@ public class movie1 {
     public static void main(String[] args) {
         System.out.println("why not working on the Hello class");
         //movie on the phone for 10
+        //final hour for movie
     }
 }
