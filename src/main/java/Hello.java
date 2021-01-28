25db7709b76d4a58f173eba4175bb65ae9a54edc
minicandoit <74155239+minicandoit@users.noreply.github.com>
2021-01-27 19:40:10 -0500

    movie 1 updated Hello.java more work B21 and B22

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Hello World,  B21");
+        System.out.println("Hello World,  B21 and B22");
         System.out.println("B21 future SDET is learning git");
     }
 }
