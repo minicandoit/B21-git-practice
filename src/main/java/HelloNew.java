0eed5661de8582d3a63612d8532f4f17c826af97
minicandoit <74155239+minicandoit@users.noreply.github.com>
2021-01-27 19:55:18 -0500

    initial HelloNew

---
+++ src/main/java/HelloNew.java
@@ -0,0 +1,6 @@
+public class HelloNew {
+    public static void main(String[] args) {
+        System.out.println("Hello World B22");
+        System.out.println("B21 future SDET learning Git");
+    }
+}
