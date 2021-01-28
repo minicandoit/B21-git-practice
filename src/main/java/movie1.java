70b483689d1dbefe1e33b7a1615fca3782540530
minicandoit <74155239+minicandoit@users.noreply.github.com>
2021-01-27 19:46:04 -0500

    watching movie for 10

--- src/main/java/movie1.java
+++ src/main/java/movie1.java
@@ -1,5 +1,6 @@
 public class movie1 {
     public static void main(String[] args) {
         System.out.println("why not working on the Hello class");
+        //whatching movie for 10 min
     }
 }
